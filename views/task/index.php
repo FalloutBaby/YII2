8c2ea72160a06613d1409c1349b2450fa0228821
FalloutBaby <ferrashe4ka@mail.ru>
2018-10-25 15:39:48 +0300

    Отображение через контроллер происходит от массива задач.

--- views/task/index.php
+++ views/task/index.php
@@ -1,8 +1,19 @@
 <div class="jumbotron">
-	<h1>Страница приветствия</h1>
-	<? if(!$username): ?>
+	<h1>Задачи</h1>
+	<? if(!$user['username']): ?>
 		<p class="lead">Пожалуйста, войдите или зарегистрируйтесь.</p>
-	<? else: ?>
-		<p class="lead">Добро пожаловать, <?= $username; ?>!</p>
-	<? endif; ?>
+		<? else: ?>
+			<p class="lead">Добро пожаловать,
+				<?= $user['username']; ?>!</p>
+			<? foreach ($tasks as $task): ?>
+			<div class="col-lg-4">
+				<h2><?= $task['title'] . ' ' . $task['id']; ?></h2>
+
+				<p><?= $task['description']; ?></p>
+
+				<p>Создана <?= $task['dateOfCreation']; ?></p>
+				<p>Выполнить до <?= $task['deadline']; ?></p>
+			</div>
+			<? endforeach; ?>
+			<? endif; ?>
 </div>
